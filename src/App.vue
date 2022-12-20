ca0e44d91f7dc92e8bd348d2ab6c455197129177
mekrograca <grkem.squash99@gmail.com>
2022-12-20 11:11:00 +0300

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="App">
+    <div>
+      <img src="../public/logo.png" width="250" height="250" alt="">
+      <h2>VUE with PWA</h2>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.App {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f2f2f2;
+  text-align: center;
 }
 </style>
